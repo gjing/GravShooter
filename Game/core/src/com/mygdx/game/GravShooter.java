9921a6e63b1d8d80596a6e7a15c63a5525ee1cac
gjing <georgejing93@gmail.com>
2016-03-09 19:57:55 -0800

    full BBeta done, scoring and game over completed

--- Game/core/src/com/mygdx/game/GravShooter.java
+++ Game/core/src/com/mygdx/game/GravShooter.java
@@ -33,7 +33,10 @@ public class GravShooter extends ApplicationAdapter {
     Circle circle2;
     Circle planet_circle;
     public int window_center_x = 400; 
-    public int window_center_y = 300; 
+    public int window_center_y = 300;
+    public int score;
+    public int ast_spawn_time;
+	private boolean game_over;
 	
 	@Override
 	public void create () {
@@ -45,11 +48,13 @@ public class GravShooter extends ApplicationAdapter {
 		defender = new Defender();
 		planet = new Planet();
 	    bullets = new Array<Bullet>();
-        circle2 = new Circle(0, 0, 15);
         planet_circle = planet.circle();
+        score = 0;
+        ast_spawn_time = 2130000000;
+        game_over = false;
 		
 		font = new BitmapFont();
-		debug = true;
+		debug = false;
 
 	    asteroids = new Array<Asteroid>();
 	    spawnAsteroid();
@@ -59,76 +64,106 @@ public class GravShooter extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		renderer.begin(ShapeRenderer.ShapeType.Filled);
-		circle2.setPosition(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
-        renderer.circle(circle2.x, circle2.y, circle2.radius);
-
-        planet.draw(renderer);
-		defender.draw(renderer);
-		for(Bullet bullet: bullets) {
-			bullet.draw(renderer);
-	    	bullet.update();
-		}
-		for(Asteroid ast: asteroids) {
-			ast.draw(renderer);
-	    	ast.update();
-		}
-
-		/*
-		 * removes bullets that have floated around for more than 60 seconds
-		 */
-		Iterator<Bullet> bullet_iter = bullets.iterator();
-	    while(bullet_iter.hasNext()) {
-	    	Bullet bullet = bullet_iter.next();
-	    	if(bullet.isExpired(TimeUtils.nanoTime())) {
-	    		bullet_iter.remove();
-	    	}
-	    }
-	    
-		/*
-		 * destroys asteroids when they collide with planet or when they collide with bullet
-		 */
-		Iterator<Asteroid> ast_iter = asteroids.iterator();
-		boolean overlap = false;
-	    while(ast_iter.hasNext()) {
-	    	Asteroid ast = ast_iter.next();
-	    	Circle ast_circle = ast.circle();
-	    	
-	    	Iterator<Bullet> bullet_hit = bullets.iterator();
-		    while(bullet_hit.hasNext()) {
-		    	Bullet bullet = bullet_hit.next();
-		    	if(ast_circle.overlaps(bullet.circle())) {
-		    		ast_iter.remove();
-		    		bullet_hit.remove();
-		    		/*Play a sound here*/
+		if (!game_over) {
+			
+			renderer.begin(ShapeRenderer.ShapeType.Filled);
+	
+	        planet.draw(renderer);
+			defender.draw(renderer);
+			for(Bullet bullet: bullets) {
+				bullet.draw(renderer);
+		    	bullet.update();
+			}
+			for(Asteroid ast: asteroids) {
+				ast.draw(renderer);
+		    	ast.update();
+			}
+	
+			/*
+			 * removes bullets that have floated around for more than 60 seconds
+			 */
+			Iterator<Bullet> bullet_iter = bullets.iterator();
+		    while(bullet_iter.hasNext()) {
+		    	Bullet bullet = bullet_iter.next();
+		    	if(bullet.isExpired(TimeUtils.nanoTime())) {
+		    		bullet_iter.remove();
 		    	}
 		    }
-	    	if(ast_circle.overlaps(planet_circle)) {
-	    		ast_iter.remove();
-	    		/*Play a sound here and put in an explosion or something*/
-	    	}
-	    }
-
-		renderer.end();
-
-		batch.begin();
-		if (debug) {
+		    
+			/*
+			 * destroys asteroids when they collide with planet or when they collide with bullet
+			 */
+			Iterator<Asteroid> ast_iter = asteroids.iterator();
+		    while(ast_iter.hasNext()) {
+		    	Asteroid ast = ast_iter.next();
+		    	Circle ast_circle = ast.circle();
+		    	
+		    	Iterator<Bullet> bullet_hit = bullets.iterator();
+			    while(bullet_hit.hasNext()) {
+			    	Bullet bullet = bullet_hit.next();
+			    	if(ast_circle.overlaps(bullet.circle())) {
+			    		ast_iter.remove();
+			    		bullet_hit.remove();
+			    		/*Play a sound here*/
+			    		score++;
+			    		if (ast_spawn_time > 130000000) {
+			    			ast_spawn_time -= 20000000;
+			    		}
+			    	}
+			    }
+		    	if(ast_circle.overlaps(planet_circle)) {
+		    		ast_iter.remove();
+		    		/*Play a sound here and put in an explosion or something*/
+		    		planet.hurt(ast.get_radius());
+		    	}
+		    }
+	
+			renderer.end();
+	
+			if(Gdx.input.isKeyPressed(Keys.LEFT)) defender.aim(-5 * Gdx.graphics.getDeltaTime());
+			if(Gdx.input.isKeyPressed(Keys.RIGHT)) defender.aim(5 * Gdx.graphics.getDeltaTime());
+			if(Gdx.input.isKeyPressed(Keys.UP)) defender.move(-5 * Gdx.graphics.getDeltaTime());
+			if(Gdx.input.isKeyPressed(Keys.DOWN)) defender.move(5 * Gdx.graphics.getDeltaTime());
+			if(TimeUtils.nanoTime() - lastShotTime > 500000000 && Gdx.input.isKeyPressed(Keys.SPACE)) {
+				bullets.add(defender.shoot());
+			}
+		    
+		    if(TimeUtils.nanoTime() - lastAstTime > ast_spawn_time) spawnAsteroid();
+		    batch.begin();
+			if (debug) {
+				font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+				font.draw(batch, defender.getAngle(), 725, 90);
+				font.draw(batch, defender.getAim(), 725, 110);
+				font.draw(batch,  String.format("health %d", planet.get_health()), window_center_x, window_center_y);
+			}
+			if (planet.get_health() < 0) {
+				game_over = true;
+			}
+			font.draw(batch, String.format("score %d", score), 10, window_center_y*2-10);
+			batch.end();
+		}
+		else {
+			batch.begin();
 			font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-			font.draw(batch, defender.getAngle(), 725, 90);
-			font.draw(batch, defender.getAim(), 725, 110);
+			font.getData().setScale(5);
+			font.draw(batch, "Game Over", 210, 350);
+			font.draw(batch, String.format("Score: %d", score), 250, 250);
+			font.getData().setScale(2);
+			font.draw(batch, "Hit Enter to Restart", 260, 170);
+			batch.end();
+			if(Gdx.input.isKeyPressed(Keys.ENTER)) {
+				score = 0;
+				defender = new Defender();
+				planet = new Planet();
+			    bullets = new Array<Bullet>();
+		        planet_circle = planet.circle();
+		        score = 0;
+		        game_over = false;
+			    asteroids = new Array<Asteroid>();
+			    spawnAsteroid();
+				font.getData().setScale(1);
+			}
 		}
-		batch.end();
-
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) defender.aim(-5 * Gdx.graphics.getDeltaTime());
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)) defender.aim(5 * Gdx.graphics.getDeltaTime());
-		if(Gdx.input.isKeyPressed(Keys.UP)) defender.move(-5 * Gdx.graphics.getDeltaTime());
-		if(Gdx.input.isKeyPressed(Keys.DOWN)) defender.move(5 * Gdx.graphics.getDeltaTime());
-		if(TimeUtils.nanoTime() - lastShotTime > 500000000 && Gdx.input.isKeyPressed(Keys.SPACE)) {
-			bullets.add(defender.shoot());
-		}
-	    
-	    if(TimeUtils.nanoTime() - lastAstTime > 2130000000) spawnAsteroid();
 	}
 	
 	public class Defender{
@@ -226,7 +261,7 @@ public class GravShooter extends ApplicationAdapter {
 		}
 	}
 	
-	public class Bullet extends Circle{
+	public class Bullet{
 		private Double x;
 		private Double y;
 		private Double vx;
@@ -323,7 +358,7 @@ public class GravShooter extends ApplicationAdapter {
 		}
 
 		public void draw(ShapeRenderer renderer) {
-			renderer.setColor(1, 0, 0, 1);
+			renderer.setColor(Color.SLATE);
 			renderer.circle(true_x(), true_y(), radius);
 		}
 		
@@ -364,6 +399,10 @@ public class GravShooter extends ApplicationAdapter {
 			return y.intValue() + window_center_y;
 		}
 		
+		public int get_radius() {
+			return radius;
+		}
+		
 		public Circle circle() {
 			return new Circle(true_x(), true_y(), 15);
 		}
@@ -378,20 +417,36 @@ public class GravShooter extends ApplicationAdapter {
 	public class Planet {
 		private int health;
 		private Color color;
+		private float r;
+		private float g;
 		
 		public Planet() {
-			health = 200;
-			color = Color.LIME;
+			health = 1000;
+			r = 0f;
+			g = 1f;
+			color = new Color(r, g, 0.2f, 0f);
 		}
 
 		public void draw(ShapeRenderer renderer) {
+			color.set(r, g, 0.2f, 0f);
 			renderer.setColor(color);
 			renderer.circle(window_center_x, window_center_y, window_center_x/4);
 		}
 		
-		public void update()
-		{
-			color = Color.BROWN;
+		public void hurt(int ast_rad) {
+			health -= ast_rad;
+			r = (float)(1.0 - health/1000.0);
+			g = (float)health/1000;
+			if (r > 1.0f) {
+				r = 1f;
+			}
+			if (g < 0f) {
+				g = 0f;
+			}
+		}
+		
+		public int get_health() {
+			return health;
 		}
 		
 		public Circle circle() {
